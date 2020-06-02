8d479ad2e430d0fbd7cdf5778879afb3f47399fc
jamie <jamie16oreilly@gmail.com>
2020-06-02 13:32:14 +0100

    init

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3.7-slim-buster
+RUN pip install --upgrade pip
+
+COPY requirements.txt /temp/
+
+RUN pip install -r requirements 
+
+RUN apt-get update && apt-get install \
+    -yqq --no-install-recommends wget, unzip
+
+ADD src /usr/local/scraper/src
+
+RUN wget -O /tmp/chromedriver.zip http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip
+RUN unzip /tmp/chromedriver.zip chromedriver -d /usr/local/bin/
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+RUN sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+RUN apt-get -y update
+RUN apt-get install -y google-chrome-stable
---
+++ src/__main__.py
@@ -0,0 +1,29 @@
+#Importing packages
+from selenium import webdriver
+import pandas as pd
+driver = webdriver.Chrome('/usr/bin/chromedriver')
+
+driver.get('https://forums.edmunds.com/discussion/40920/ford/f-150/fuel-economy-test-2-7-liter-ecoboost-vs-5-0-liter-v8-2015-ford-f-150-long-term-road-test')
+
+comments = pd.DataFrame(columns=['Date', 'user_id', 'comments'])
+ids = driver.find_elements_by_xpath("//*[contains(@id,'Comment_')]")
+comment_ids = []
+for i in ids:
+    comment_ids.append(i.get_attribute('id'))
+
+for x in comment_ids:
+    # Extract dates from for each user on a page
+    user_date = driver.find_elements_by_xpath('//*[@id="' + x + '"]/div/div[2]/div[2]/span[1]/a/time')[0]
+    date = user_date.get_attribute('title')
+
+    # Extract user ids from each user on a page
+    userid_element = driver.find_elements_by_xpath('//*[@id="' + x + '"]/div/div[2]/div[1]/span[1]/a[2]')[0]
+    userid = userid_element.text
+
+    # Extract Message for each user on a page
+    user_message = driver.find_elements_by_xpath('//*[@id="' + x + '"]/div/div[3]/div/div[1]')[0]
+    comment = user_message.text
+
+    # Adding date, userid and comment for each user in a dataframe
+    comments.loc[len(comments)] = [date, userid, comment]
+driver.quit()
